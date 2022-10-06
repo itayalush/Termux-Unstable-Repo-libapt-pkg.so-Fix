d6d66ec4e34492848c3a97571e3b8ff9f2c22b64
itayalush <64968780+itayalush@users.noreply.github.com>
2022-10-06 16:05:38 +0300

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -10,10 +10,10 @@ fi
 curl --slient http://mirrors.sdu.edu.cn/termux/termux-main/pool/main/w/wget/wget_1.21.3-3_"$(uname -m)".deb -o wget.deb
 dpkg -i wget.deb | grep literallynothing
 rm wget.deb
-wget https://github.com/itayalush/Termux-Unstable-Repo-libapt-pkg.so-Fix/releases/download/"$(uname -m)"/apt_termux.deb | sed "s,$,(echo "Downloading apt..."),"
-wget https://github.com/itayalush/Termux-Unstable-Repo-libapt-pkg.so-Fix/releases/download/"$(uname -m)"/libc++_termux.deb | sed "s,$,(echo "Downloading libc++..."),"
+wget https://github.com/itayalush/Termux-Unstable-Repo-libapt-pkg.so-Fix/releases/download/"$(uname -m)"/apt_termux.deb | sed "$,(echo "Downloading apt..."),"
+wget https://github.com/itayalush/Termux-Unstable-Repo-libapt-pkg.so-Fix/releases/download/"$(uname -m)"/libc++_termux.deb | sed "$,(echo "Downloading libc++..."),"
 echo "Installing..."
-dpkg -i apt_termux.deb | sed "s,$,(echo "Installing apt..."),"
-dpkg -i libc++_termux.deb | sed "s,$,(echo "Installing libc++..."),"
+dpkg -i apt_termux.deb | sed "$,(echo "Installing apt..."),"
+dpkg -i libc++_termux.deb | sed "$,(echo "Installing libc++..."),"
 rm apt_termux.deb
 rm libc++_termux.deb
